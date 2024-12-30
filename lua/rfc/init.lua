3b0d27b01088667b03a950bc91c55810f7ab0b4c
Cyber Oliveira <lccro@msn.com>
2024-12-30 20:07:21 -0300

    chore(tests): passing useful tests!

--- lua/rfc/init.lua
+++ lua/rfc/init.lua
@@ -1,41 +1,54 @@
-local curl = require('plenary.curl')
-local pickers = require "telescope.pickers"
-local finders = require "telescope.finders"
-local previewers = require "telescope.previewers"
-local conf = require("telescope.config").values
-local actions = require "telescope.actions"
-local action_state = require "telescope.actions.state"
+local M = {
+  index_url = "https://www.ietf.org/rfc/rfc-ref.txt",
+  rfc_url_fmt = "https://www.ietf.org/rfc/%s.txt"
+}
 
-local M = {}
+M.rfc_url = function(rfc) return string.format(M.rfc_url_fmt, rfc) end
 
-function M.picker(_, opts)
-  opts = opts or {}
-
-  local response = curl.get("https://www.ietf.org/rfc/rfc-ref.txt", { compressed = true })
-  local split_opts = { plain = true, trimempty = false }
-  local split = vim.split(response.body, "\n\n", split_opts)
-  local results = vim.split(split[2], "\n", split_opts)
-
+M.parse_results = function(results)
   for i, result in ipairs(results) do
+    -- RFC0001 |           | Crocker, S., "Host Software", RFC 1, DOI 10.17487/RFC0001, April 1969, <https://www.rfc-editor.org/info/rfc1>.
     local cols = vim.split(result, "|", { plain = true, trimempty = true })
-    local code = cols[1]
+    local code = vim.trim(cols[1])
     local code_nr = tonumber(code:match("(%d+)"))
     local title = string.format("%d. %s", code_nr, cols[3]:match([["(.+)"]]))
     local value = string.format("rfc%d", code_nr)
 
-    ---@diagnostic disable-next-line: assign-type-mismatch
     results[i] = { code, title, value }
   end
+  return results
+end
 
-  local function get_rfc(rfc)
-    local rfc_response = curl.get(string.format("https://www.ietf.org/rfc/%s.txt", rfc), { compressed = true })
-    return vim.split(rfc_response.body, "\n", { plain = true, trimempty = false })
-  end
+M.download_all = function()
+  local curl = require('plenary.curl')
+  local response = curl.get(M.index_url, { compressed = true })
+  local split_opts = { plain = true, trimempty = false }
+  local split = vim.split(response.body, "\n\n", split_opts)
+  local results = vim.split(split[2], "\n", split_opts)
+  return M.parse_results(results)
+end
+
+M.get_rfc = function(rfc)
+  local curl = require('plenary.curl')
+  local url = M.rfc_url(rfc)
+  local rfc_response = curl.get(url, { compressed = true })
+  return vim.split(rfc_response.body, "\n", { plain = true, trimempty = false })
+end
+
+M.picker = function(_, opts)
+  opts = opts or {}
+
+  local pickers = require "telescope.pickers"
+  local finders = require "telescope.finders"
+  local previewers = require "telescope.previewers"
+  local conf = require("telescope.config").values
+  local actions = require "telescope.actions"
+  local action_state = require "telescope.actions.state"
 
   pickers.new(opts, {
     prompt_title = "RFCs",
     finder = finders.new_table {
-      results = results,
+      results = M.download_all(),
       entry_maker = function(entry)
         return {
           value = entry[3],
@@ -47,8 +60,8 @@ function M.picker(_, opts)
     previewer = previewers.new_buffer_previewer {
       title = opts.preview_title or "RFC preview",
       define_preview = function(self, entry)
-        local lines = get_rfc(entry.value)
-        vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, lines)
+        vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false,
+          M.get_rfc(entry.value))
       end
     },
     sorter = conf.generic_sorter(opts),
@@ -56,16 +69,25 @@ function M.picker(_, opts)
       actions.select_default:replace(function()
         actions.close(prompt_bufnr)
         local selection = action_state.get_selected_entry()
-        local lines = get_rfc(selection.value)
 
-        -- create a new tab and paste it into it
         local bufnr = vim.api.nvim_create_buf(false, true)
+        local width = 80
+        local col = (vim.o.columns - width) / 2
+        local nlines = vim.o.lines - 4
+
+        -- create a floating window
         local win = vim.api.nvim_open_win(bufnr, false,
-          { relative = 'win', row = 3, col = (vim.o.columns - 80) / 2, width = 80, height = vim.o.lines - 6 })
+          { relative = 'win', row = 1, col = col, width = width, height = nlines })
         vim.api.nvim_win_set_buf(win, bufnr)
         vim.api.nvim_set_current_win(win)
-        vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, lines)
+        -- paste contents in
+        vim.api.nvim_buf_set_lines(bufnr, 0, -1, false,
+          M.get_rfc(selection.value))
+        -- window and buffer local opts
         vim.wo[win].number = false
+        vim.bo[bufnr].modifiable = false
+        -- use 'q' to close the popup
+        vim.keymap.set('n', 'q', '<cmd>close<cr>', { buffer = bufnr })
       end)
       return true
     end,
--- lua/tests/rfc/plugin_spec.lua
+++ lua/tests/rfc/plugin_spec.lua
@@ -1,12 +1,31 @@
 local rfc = require('rfc.init')
 local assert = require('luassert')
+local eq = assert.are.same
 
 -- lua vim.keymap.set('n', '<leader>t', '<cmd>w | PlenaryBustedDirectory lua/tests/ {init="lua/tests/minimal.lua"}<cr>')
 
-describe('rfc neovim plugin', function()
-  it('work as expect', function()
-    local result = rfc.picker()
-    assert.is_nil(result)
-    assert.are.same(result, nil)
+describe('rfc.nvim plugin', function()
+  it('creates an rfc url', function()
+    eq("https://www.ietf.org/rfc/123.txt", rfc.rfc_url(123))
+  end)
+
+  it('properly parses an rfc line', function()
+    local lines = {
+      [[RFC0001 |           | Crocker, S., "Host Software", RFC 1, DOI 10.17487/RFC0001, April 1969, <https://www.rfc-editor.org/info/rfc1>.]]
+    }
+    local actual = rfc.parse_results(lines)
+    eq('table', type(actual))
+    eq({ { 'RFC0001', '1. Host Software', 'rfc1' } }, actual)
+  end)
+
+  it('downloads all rfcs', function()
+    eq('table', type(rfc.download_all()))
+  end)
+
+  it('downloads a given rfc', function()
+    local actual = rfc.get_rfc('rfc1')
+    local expected = "Request for Comments: 1                                          UCLA"
+    eq('table', type(actual))
+    assert.is_true(vim.tbl_contains(actual, expected))
   end)
 end)
